853664adbb727590652664dc731519961f05833e
Leo0liveira <leoufms@gmail.com>
2021-01-26 00:09:06 -0400

    feat: gravidade e troca de telas

--- jogo.js
+++ jogo.js
@@ -71,6 +71,13 @@ const flappyBird = {
   altura: 24,
   x: 10,
   y: 50,
+  gravidade: 0.25,
+  velocidade: 0,
+
+  atualiza(){
+      flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+    flappyBird.y = flappyBird.y + 1;
+  },
   desenha() {
     contexto.drawImage(
       sprites,
@@ -81,15 +88,68 @@ const flappyBird = {
     );
   }
 }
+const mensagemGetReady = {
+    sX:134,
+    sY:0,
+    w: 174,
+    h:152,
+    x: (canvas.width / 2 ) -174/2,
+    y: 50,
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            mensagemGetReady.sX, mensagemGetReady.sY,
+            mensagemGetReady.w, mensagemGetReady.h,
+            mensagemGetReady.x, mensagemGetReady.y,
+            mensagemGetReady.w, mensagemGetReady.h
+        );
+    }
+}
+let telaAtiva = {};
+function mudaParaTela(novaTela){
+    telaAtiva = novaTela;
 
-function loop() {
-  planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
-
-  flappyBird.y = flappyBird.y + 1;
-
-  requestAnimationFrame(loop);
+}
+const Telas = {
+    INICIO: {
+        desenha(){
+            planoDeFundo.desenha();
+            chao.desenha();
+            flappyBird.desenha();
+            mensagemGetReady.desenha();
+            mensagemGetReady.desenha();;
+        },
+        click(){
+            mudaParaTela(Telas.JOGO);
+        },
+        atualiza(){
+             
+        }
+    }
+};
+Telas.JOGO = {
+    desenha(){
+        planoDeFundo.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+    },
+    atualiza(){
+        flappyBird.atualiza();
+    }
 }
 
+function loop() {
+    telaAtiva.desenha();
+    telaAtiva.atualiza();
+
+    requestAnimationFrame(loop);
+}
+
+window.addEventListener('click',function(){
+    if (telaAtiva.click) {
+        telaAtiva.click();
+    }    
+});
+
+mudaParaTela(Telas.INICIO);
 loop();
